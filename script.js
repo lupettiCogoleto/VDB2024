347f3ba6122435c6bb59cf45df59dbaf7a9c59ea
lupettiCogoleto <lupetticogoleto1@gmail.com>
2024-07-06 21:36:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById('password');
+    var toggleIcon = document.querySelector('.toggle-password');
+
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        toggleIcon.classList.remove('fa-eye-slash');
+        toggleIcon.classList.add('fa-eye');
+    } else {
+        passwordInput.type = 'password';
+        toggleIcon.classList.remove('fa-eye');
+        toggleIcon.classList.add('fa-eye-slash');
+    }
+}
+
+const injectedDiv = document.querySelector('div[style*="z-index:9999999;bottom: 0;width: auto;right: 1%;"]');
+if (injectedDiv) {
+    injectedDiv.remove();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('login-form');
+    const loginDiv = document.getElementById('login');
+    const passwordInput = document.getElementById('password');
+    const usernameInput = document.getElementById('username');
+    
+    const injectedDiv = document.querySelector('div[style*="z-index:9999999;bottom: 0;width: auto;right: 1%;"]');
+    if (injectedDiv) {
+        injectedDiv.remove();
+    }
+    
+    // Function to fetch and validate users
+    async function fetchAndValidateUsers(username, password) {
+        try {
+            const response = await fetch('users.json');
+            const users = await response.json();
+
+            // Check if the entered username and password match any user in the JSON file
+            // Make username not case sensitive
+            username = username.toLowerCase();
+            const isValidUser = users.some(user => user.username === username && user.password === password);
+
+            if (isValidUser) {
+                localStorage.setItem('loggedIn', 'true');
+                localStorage.setItem('username', username);
+                window.location.href = 'home-image.html';
+            } else {
+                loginDiv.classList.add('shake');
+                setTimeout(() => {
+                    loginDiv.classList.remove('shake');
+                }, 300);
+
+                passwordInput.classList.add('invalid');
+                usernameInput.classList.add('invalid');
+                setTimeout(() => {
+                    passwordInput.classList.remove('invalid');
+                    usernameInput.classList.remove('invalid');
+                }, 500);
+
+                passwordInput.value = '';
+            }
+        } catch (error) {
+            console.error('Error fetching users:', error);
+            alert('An error occurred. Please try again.');
+        }
+    }
+    if (window.location.pathname.includes('index.html')){
+        loginForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+            const username = document.getElementById('username').value;
+            const password = document.getElementById('password').value;
+    
+            fetchAndValidateUsers(username, password);
+        });
+    }
+    
+    if (window.location.pathname.includes('home-image.html')) {
+        const imagePath = `images/${localStorage.getItem('username')}.png`; // Example path to your images
+        localStorage.setItem('username', "");
+
+        setImage(imagePath);
+    }
+
+    function setImage(imagePath) {
+        const imageContainer = document.getElementById('imageContainer');
+        const img = document.createElement('img');
+        
+        img.src = imagePath;
+        img.alt = 'User Image'; // Optional: Set alt attribute for accessibility
+
+        // Clear previous content (if any) and append new image
+        imageContainer.innerHTML = '';
+        imageContainer.appendChild(img);
+    }
+});
